c51fd6affc6774fae50d7d9df1e44acd05a4bb97
CAMILA RIOS <ca.riosi@duocuc.cl>
2024-12-13 18:26:01 -0300

    diseno-final

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,16 +1,21 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, Input} from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   template: `
-    <ion-searchbar (ionInput)="onInput($event)"></ion-searchbar>
+    <ion-searchbar 
+      (ionInput)="onInput($event)" 
+      class="bobstrap-searchbar"
+      [placeholder]="placeholder">
+    </ion-searchbar>
   `,
 })
 export class SearchBarComponent {
+  @Input() placeholder: string = 'Buscar'; // Valor predeterminado
   @Output() searchTermChanged = new EventEmitter<string>();
 
   onInput(event: Event): void {
     const inputElement = event.target as HTMLInputElement;
-    this.searchTermChanged.emit(inputElement.value); // Emitir el valor del input como string
+    this.searchTermChanged.emit(inputElement.value);
   }
 }
